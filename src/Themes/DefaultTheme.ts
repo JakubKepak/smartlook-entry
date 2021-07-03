15d37e1422efe1df271a3418b115f972b733ef60
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-07-03 16:32:42 +0200

    add theme

--- src/Themes/DefaultTheme.ts
+++ src/Themes/DefaultTheme.ts
@@ -1,3 +1,17 @@
 export const defaultTheme = {
-  backgroundColor: 'red',
+  colors: {
+    backgroundColorRoot: '#F2F2F2',
+    backgroundColorItem: '#fff',
+    backgroundHeader: '#353E66',
+    fontColorDark: '#333F66',
+    fontColorLight: '#fff',
+  },
+
+  fontWeight: {
+    normal: '300',
+    bold: '500',
+    extraBold: '700',
+  },
+
+  borderRadius: '5px',
 };
--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -2,6 +2,20 @@ import '@emotion/react';
 
 declare module '@emotion/react' {
   export interface Theme {
-    backgroundColor: string;
+    colors: {
+      backgroundColorRoot: string;
+      backgroundColorItem: string;
+      backgroundHeader: string;
+      fontColorDark: string;
+      fontColorLight: string;
+    };
+
+    fontWeight: {
+      normal: string;
+      bold: string;
+      extraBold: string;
+    };
+
+    borderRadius: string;
   }
 }
