0987221a6a9a8320cff91006c914d4aff146e21c
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-07-03 16:34:02 +0200

    add Post detail

---
+++ src/components/PostPreview/Styles.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const MainContainer = styled('div')`
+  background-color: ${({ theme }) => theme.colors.backgroundColorItem};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  padding: 2rem;
+  width: 100%;
+  box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.1);
+`;
+
+export const InnerContainer = styled('div')`
+  display: grid;
+  grid-gap: 1rem;
+`;
+
+export const Title = styled('span')`
+  font-weight: ${({ theme }) => theme.fontWeight.extraBold};
+`;
+
+export const Body = styled('div')`
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+`;
