d056d7ab9b31ce9fb16cd422ff6f961a6b309401
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-07-03 16:33:03 +0200

    add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import * as S from './Styles';
+
+export default function Header(): React.ReactElement {
+  return (
+    <S.MainContainer>
+      <S.Title>Test</S.Title>
+    </S.MainContainer>
+  );
+}
---
+++ src/components/Header/Styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const MainContainer = styled('div')`
+  background-color: ${({ theme }) => theme.colors.backgroundHeader};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  padding: 1rem;
+  width: 100%;
+`;
+
+export const Title = styled('div')`
+  color: ${({ theme }) => theme.colors.fontColorLight};
+  font-weight: ${({ theme }) => theme.fontWeight.extraBold};
+`;
