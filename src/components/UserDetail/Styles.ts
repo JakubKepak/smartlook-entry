62540695c384b35ada2c4b4cd8331b448bf5e2a6
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-07-04 19:06:27 +0200

    add user detail sceleton

--- src/components/UserDetail/Styles.ts
+++ src/components/UserDetail/Styles.ts
@@ -1,1 +1,37 @@
 import styled from '@emotion/styled';
+
+export const MainContainer = styled.div`
+  background-color: ${({ theme }) => theme.colors.backgroundColorItem};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  padding: 2rem;
+  width: 100%;
+  box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.1);
+`;
+
+export const DetailsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+`;
+
+export const DetailSectionContainer = styled.div`
+  display: grid;
+  grid-gap: 0.7rem;
+  align-content: start;
+`;
+
+export const MapContainer = styled.div`
+  margin-top: 1rem;
+`;
+
+export const DetailItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const DetailItem = styled.span``;
+
+export const DetailItemLabel = styled.span`
+  font-size: ${({ theme }) => theme.fontSize.small};
+  color: ${({ theme }) => theme.colors.fontColorGrey};
+`;
--- src/components/UserDetail/UserDetail.tsx
+++ src/components/UserDetail/UserDetail.tsx
@@ -1,17 +1,62 @@
-import { UserInterface } from 'Types/User'
-import * as S from './Styles'
+import { UserInterface } from 'Types/User';
+import * as S from './Styles';
 
 interface Props {
-    user: UserInterface
+  user: UserInterface;
 }
 
-export default function UserDetail({user}: Props): React.ReactElement {
+export default function UserDetail({ user }: Props): React.ReactElement {
+  return (
+    <S.MainContainer>
+      <S.DetailsContainer>
+        <S.DetailSectionContainer>
+          <S.DetailItemContainer>
+            <S.DetailItemLabel>Name</S.DetailItemLabel>
+            <S.DetailItem>{user.name}</S.DetailItem>
+          </S.DetailItemContainer>
+          <S.DetailItemContainer>
+            <S.DetailItemLabel>Username</S.DetailItemLabel>
+            <S.DetailItem>{user.username}</S.DetailItem>
+          </S.DetailItemContainer>
+          <S.DetailItemContainer>
+            <S.DetailItemLabel>E-mail</S.DetailItemLabel>
+            <S.DetailItem>{user.email}</S.DetailItem>
+          </S.DetailItemContainer>
+        </S.DetailSectionContainer>
+
+        <S.DetailSectionContainer>
+          <S.DetailItemContainer>
+            <S.DetailItemLabel>Street</S.DetailItemLabel>
+            <S.DetailItem>{user.address.street}</S.DetailItem>
+          </S.DetailItemContainer>
+          <S.DetailItemContainer>
+            <S.DetailItemLabel>Suite</S.DetailItemLabel>
+            <S.DetailItem>{user.address.suite}</S.DetailItem>
+          </S.DetailItemContainer>
+          <S.DetailItemContainer>
+            <S.DetailItemLabel>City</S.DetailItemLabel>
+            <S.DetailItem>{user.address.city}</S.DetailItem>
+          </S.DetailItemContainer>
+          <S.DetailItemContainer>
+            <S.DetailItemLabel>Zipcode</S.DetailItemLabel>
+            <S.DetailItem>{user.address.zipcode}</S.DetailItem>
+          </S.DetailItemContainer>
+        </S.DetailSectionContainer>
+
+        <S.DetailSectionContainer>
+        <S.DetailItemContainer>
+            <S.DetailItemLabel>Company name</S.DetailItemLabel>
+            <S.DetailItem>{user.company.name}</S.DetailItem>
+          </S.DetailItemContainer>    
+        <S.DetailItemContainer>
+            <S.DetailItemLabel>Catch phrase</S.DetailItemLabel>
+            <S.DetailItem>{user.company.catchPhrase}</S.DetailItem>
+          </S.DetailItemContainer>
+        </S.DetailSectionContainer>
 
 
-
-    return (
-        <div>
-            {user.name}
-        </div>
-    )
+      </S.DetailsContainer>
+      <S.MapContainer>Map</S.MapContainer>
+    </S.MainContainer>
+  );
 }
