0b557ae0f38b8e6b2f2cba35f8531c4f48cbcb8c
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-07-05 10:35:00 +0200

    add breakpoints

---
+++ src/breakpoints.ts
@@ -0,0 +1,11 @@
+export const size = {
+  xs: '380px',
+  sm: '768px',
+  lg: '1200px',
+};
+
+export const device = {
+  xs: `(max-width: ${size.xs})`,
+  sm: `(max-width: ${size.sm})`,
+  lg: `(max-width: ${size.lg})`,
+};
