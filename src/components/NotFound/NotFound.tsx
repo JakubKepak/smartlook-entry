27b723a43d8e13281b9741fe85ab35a055690ff5
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-07-05 11:44:12 +0200

    update 404 page

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,17 +1,34 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+// components
+import notFoundIllustration from 'assets/notfound.svg';
 
 const MainContainer = styled.div`
   width: 100%;
   height: 90vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const NotFoundImage = styled.img`
+  width: 300px;
+  height: 300px;
+`;
+
+const GoBackContainer = styled.div`
+    font-weight: ${({theme}) => theme.fontWeight.extraBold};
 `;
 
 export default function NotFound(): React.ReactElement {
   return (
     <MainContainer>
-      <span>404</span>
+      <NotFoundImage src={notFoundIllustration} />
+      <GoBackContainer>
+        <Link to='/'>Take me home</Link>
+      </GoBackContainer>
     </MainContainer>
   );
 }
